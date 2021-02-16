7a3b0515abf3f6f48cfee2d255cff6d7aa947797
namaninaresh <namaninaresh505@gmail.com>
2021-02-16 14:50:06 +0530

    gallery and pricing Defined

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,27 @@
 
-
 const menu = document.querySelectorAll(".toggler");
 menu.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("menu-expanded");
   })
 );
+
+
+/*const videos = document.querySelectorAll(".video-js");
+
+videos.forEach((item)=> item.addEventListener("mouseover", function(){
+    this.play();
+}),
+ 
+); */
+
+
+
+const VideoController = function(isHovering, videoElement)
+{
+  if(isHovering) videoElement.play();
+  else if(isHovering == false)
+  {
+    videoElement.pause();
+  }
+}
